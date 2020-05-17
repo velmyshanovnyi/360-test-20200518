eefbb0d4139f0aeeebfe116a8c1ea9d21aa7ffcb
ANT <velmyshanovnyi@gmail.com>
2020-05-18 02:33:26 +0300

    0-

---
+++ data.js
@@ -0,0 +1,211 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "балкон",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": -0.334770940296643,
+        "pitch": -0.24026594970874804,
+        "fov": 1.1490439151539336
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8398070613277646,
+          "pitch": 0.5520347921186044,
+          "rotation": 6.283185307179586,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.188487904408337,
+          "pitch": 0.4326983442087027,
+          "title": "парковка",
+          "text": "завжди є місце щоб поставити машину..."
+        },
+        {
+          "yaw": -1.3444607455456072,
+          "pitch": -0.11264161843213572,
+          "title": "магазин",
+          "text": "там їжа...."
+        },
+        {
+          "yaw": -0.017858099607932942,
+          "pitch": -0.3283422305400645,
+          "title": "схід сонця",
+          "text": "зранку дуже красиво..."
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "кухня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": -1.0604349533096098,
+        "pitch": 0.3414039036686578,
+        "fov": 1.0965716292348757
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1327390032153275,
+          "pitch": 0.29572055554418597,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.9318473797390663,
+          "pitch": 0.2598819453782344,
+          "rotation": 1.5707963267948966,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3634406042911724,
+          "pitch": 0.5816780977051863,
+          "title": "пралька",
+          "text": "...тихесенька :)"
+        },
+        {
+          "yaw": -2.5552891670349354,
+          "pitch": 0.6154930540025809,
+          "title": "плита",
+          "text": "завжди смачно виходить...."
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "коридор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": -1.3320004403575538,
+        "pitch": 0.4553274547604449,
+        "fov": 1.1490439151539336
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.097298480684639,
+          "pitch": 0.19648644883176658,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.8721610927105345,
+          "pitch": 0.8896254980611857,
+          "rotation": 6.283185307179586,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.412015425187521,
+          "pitch": 0.056493336542793315,
+          "title": "вхідні двері",
+          "text": "сусідів не чути...."
+        },
+        {
+          "yaw": 0.7982593318238234,
+          "pitch": 0.3050102282523639,
+          "title": "санвузел",
+          "text": "все стандартно..."
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "кімната",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": 2.388996612834273,
+        "pitch": 0.5050196127467927,
+        "fov": 1.1490439151539336
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8932647485014842,
+          "pitch": 0.32340884816452053,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1520538512420195,
+          "pitch": 0.7912314500149087,
+          "title": "робоче місце",
+          "text": "тут зручно працювати і гратись в танки :)"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
